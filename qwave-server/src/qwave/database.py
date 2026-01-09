d35e044855e44fc4f4822d31a5e3e4ac1118de0a
qwikster <ocoincs@gmail.com>
2026-01-08 19:29:10 -0500

    database.py imports (aka literally anything)

---
+++ qwave-server/src/qwave/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Generator
+
+from qwave.models import Base
+# from qwave.config import get_config # TODO
+
+_engine = None
+_SessionLocal = None
+
+def init_db(database_url: str):
+    global _engine, _SessionLocal
+    
+    # etc
+    
+    return _engine
